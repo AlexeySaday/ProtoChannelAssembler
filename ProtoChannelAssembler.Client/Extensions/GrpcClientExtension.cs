2cc7c3fae997fc2ef61b5e375f058c953185ffcd
alexeikildyushev@gmail.com <alexeikildyushev@gmail.com>
2023-09-30 14:44:17 +0300

    add services

---
+++ ProtoChannelAssembler.Client/Extensions/GrpcClientExtension.cs
@@ -0,0 +1,11 @@
+using Grpc.Net.Client;
+
+namespace Channel.Extensions;
+
+public static class GrpcClientExtension
+{
+    // public static IHttpClientBuilder AddGrpcHttpClient(this IServiceCollection services)
+    // {
+    //     var channel = GrpcChannel.ForAddress("https://localhost:5001");
+    // }
+}
